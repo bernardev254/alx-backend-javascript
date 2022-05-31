53e58483fd5e22213532dc36c1517f0cc86cd426
bkaraba <bkaraba14@gmail.com>
2022-05-31 14:31:44 +0000

    1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,13 @@
 console.log("Welcome to Holberton School, what is your name?");
+
 process.stdin.on('readable', () => {
 	let answer = process.stdin.read();
 	if (answer !== null) {
-		process.stdout.write("Your name is: " + answer);
+		process.stdout.write(`Your name is: ${answer}`);
 	}
 	answer = process.argv[1];
 });
+
 process.stdin.on('end', () => {
 	process.stdout.write("This important software is now closing\n");
 });
